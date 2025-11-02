bfbc4e854c06db81354a643ef8b98d80afa93f20
bnr1432 <nagarjunar321@gmail.com>
2025-11-02 08:07:47 +0530

    Changed email js/browser

--- Frontend/frontend/src/pages/Contact.jsx
+++ Frontend/frontend/src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from '@emailjs/browser';
 import Footer from "../Components/Footer";
 
 export default function Contact() {
